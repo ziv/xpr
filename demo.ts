112a1faf9c50ecf3dedbb8026f5638c726e6673a
Ziv <ziv.perry@zoominfo.com>
2022-02-01 21:12:58 +0200

    no imports

---
+++ demo.ts
@@ -0,0 +1,40 @@
+import "./core/reflection/mod.ts";
+import { Context, Inject, Injectable, Module } from "./core/mod.ts";
+
+@Injectable()
+class SizeService {
+  constructor(@Inject("size") public i: number) {
+  }
+}
+
+const SizeToken = { token: "size", factory: () => 10 };
+
+@Module({
+  providers: [
+    SizeToken,
+    SizeService
+  ],
+  exports: [SizeToken, SizeService]
+})
+class SizeModule {
+}
+
+const ConfiguredSizeToken = { token: "size", factory: () => 20 };
+
+@Module({
+  imports: [SizeModule],
+  providers: [ConfiguredSizeToken],
+  exports: [SizeModule]
+})
+class ConfiguredServiceModule {
+}
+
+@Module({
+  imports: [ConfiguredServiceModule]
+})
+class App {
+}
+
+const app = await Context.create(App, { strict: true });
+const service = await app.mod.resolve<SizeService>(SizeService);
+console.log(service);
