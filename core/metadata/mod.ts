22d46863a06f9b649b6205365386ba9b78ee4a16
Ziv <ziv.perry@zoominfo.com>
2022-02-07 11:54:52 +0200

    test: get-ctr-params

--- core/metadata/mod.ts
+++ core/metadata/mod.ts
@@ -1,5 +1,5 @@
 import addParam from "./add_param.ts";
-import getCtrParams from "./get-ctr-params.ts";
+import getCtrParams from "./get_ctr_params.ts";
 import getInjectable from "./get-injectable.ts";
 import getInjectedParams from "./get-injected-params.ts";
 import getModuleDefinition from "./get-module-definition.ts";
