888449f470cd067f406f7e9f4915351daab62699
Ziv <ziv.perry@zoominfo.com>
2022-02-07 12:25:09 +0200

    test: get injected params

--- lib/core/internals/provider.ts
+++ lib/core/internals/provider.ts
@@ -1,9 +1,9 @@
 import type { FactoryProvider, Func, Resolver, Target } from "core/types/mod.ts";
-import { getCtrParams, getInjectable, getInjectedParams } from "core/metadata/mod.ts";
+import { getCtrParams, getInjectable, get_injected_params } from "core/metadata/mod.ts";
 
 export default function provider(target: Target): FactoryProvider {
   const ctrParams = getCtrParams(target);
-  const injectedParams = getInjectedParams(target);
+  const injectedParams = get_injected_params(target);
   const injectable = getInjectable(target);
 
   for (const { index, value } of injectedParams) {
